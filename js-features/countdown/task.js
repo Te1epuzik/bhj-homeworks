8abe9907db8667c3267b4acc3901692d4cbeb669
Neto test <test@neto.com>
2024-03-25 15:54:54 +0300

    HomeWork 1.1

---
+++ js-features/countdown/task.js
@@ -0,0 +1,31 @@
+"use strict"
+
+function getTime() {
+	const timer = document.getElementById("timer");
+	let hours = +timer.textContent.slice(0, 2);
+	let minutes = +timer.textContent.slice(3, 5);
+	let seconds = +timer.textContent.slice(-2);
+	let time = hours * 3600 + minutes * 60 + seconds;
+	return time;
+}
+
+(function timerStart() {
+	let time = getTime();
+	let hours;
+	let minutes;
+	let seconds;
+	setInterval(() => {
+		if (time === 0) {
+			alert("Вы победили в конкурсе!");
+			return;
+		}
+		time--;
+		hours = Math.floor(time / 3600);
+		hours = hours < 10 ? "0" + hours : hours;
+		minutes = Math.floor((time - hours * 3600) / 60);
+		minutes = minutes < 10 ? "0" + minutes : minutes;
+		seconds = time % 60;
+		seconds = seconds < 10 ? "0" + seconds : seconds;
+		timer.innerHTML = `${hours}:${minutes}:${seconds}`
+	}, 1000)
+})();
