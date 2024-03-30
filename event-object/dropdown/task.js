f80b7324b38d650cf9d325fa4203a28b1802321e
Neto test <test@neto.com>
2024-03-30 14:44:39 +0300

    HomeWork event objects 1

---
+++ event-object/dropdown/task.js
@@ -0,0 +1,23 @@
+"use strict";
+
+(function dropDownList() {
+	const dropDownMenu = document.querySelector(".dropdown__value");
+	const dropDownList = document.querySelector(".dropdown__list");
+	const dropDownItems = Array.from(document.querySelectorAll(".dropdown__link"));
+
+	dropDownMenu.addEventListener("click", () => {
+		if (dropDownList.className.includes("dropdown__list_active")) {
+			dropDownList.classList.remove("dropdown__list_active");
+			return;
+		};
+
+		dropDownList.className += " dropdown__list_active";
+	});
+
+	for (let dropDownItem of dropDownItems) {
+		dropDownItem.addEventListener("click", () => {
+			dropDownMenu.innerHTML = dropDownItem.textContent;
+			dropDownList.classList.remove("dropdown__list_active");
+		});
+	};
+}());
