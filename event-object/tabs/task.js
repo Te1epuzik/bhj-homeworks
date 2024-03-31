3e82771eaba603228adbfef525908ab3d5aebea4
Neto test <test@neto.com>
2024-03-31 23:44:30 +0300

    HomeWork event objects correction 3

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -1,15 +1,24 @@
-"use strict"; 
+"use strict";
 
-(function switchTab() {
-	const tabs = Array.from(document.querySelectorAll(".tab"));
-	const tabContents = Array.from(document.querySelectorAll(".tab__content"));
+class Tabs {
+	constructor(container) {
+		this.container = container;
+		this.tabs = Array.from(container.querySelectorAll(".tab"));
+		this.tabContents = Array.from(container.querySelectorAll(".tab__content"));
 
-	tabs.forEach(tab => {
-		tab.addEventListener("click", () => {
-			tabs.forEach(tab => tab.classList.remove("tab_active"));
-			tab.classList.add("tab_active");
-			tabContents.forEach(content => content.classList.remove("tab__content_active"));
-			tabContents[tabs.indexOf(tab)].classList.add("tab__content_active")
+		this.switchTab();
+	}
+
+	switchTab() {
+		this.tabs.forEach(tab => {
+			tab.addEventListener("click", () => {
+				this.tabs.forEach(tab => tab.classList.remove("tab_active"));
+				tab.classList.add("tab_active");
+				this.tabContents.forEach(content => content.classList.remove("tab__content_active"));
+				this.tabContents[this.tabs.indexOf(tab)].classList.add("tab__content_active")
+			});
 		});
-	});
-}())
+	}
+}
+
+new Tabs(document.getElementById("tabs1"));
