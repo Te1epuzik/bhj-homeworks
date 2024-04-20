71109f90bf5ff672a9bdefc80786936e7e41cce8
Neto test <test@neto.com>
2024-04-20 19:07:21 +0300

    HomeWork document srtucture task 3

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -17,13 +17,14 @@ class Shop {
 class Cart {
 	static container = document.querySelector('.cart');
 	static cartProducts = document.querySelector('.cart__products');
-	static id = [];
-	static deleteBtns = Array.from(document.querySelectorAll('.cart__product-delete'));
+	static productId = [];
 
 	static checkCart() {
-		if (Cart.id.length !== 0) {
+		if (this.productId.length !== 0) {
 			this.container.classList.add('cart__active');
-		} else this.container.classList.remove('cart__active');
+		} else {
+			this.container.classList.remove('cart__active');
+		}
 	}
 
 	static getQuantity(productId) {
@@ -34,30 +35,38 @@ class Cart {
 	}
 
 	static addProduct(productId, image, quantity) {
-		if (this.id.includes(productId)) {
+		if (this.productId.includes(productId)) {
 			this.cartProducts
 				.querySelector(`[data-id="${productId}"]`)
 				.querySelector('.cart__product-count')
 				.textContent = +this.getQuantity(productId) + quantity;
 			
-			return;
+			return this.cartProducts;
 		}
 
-		this.id.push(productId);
-		this.cartProducts.insertAdjacentHTML('beforeEnd', 
+		this.productId.push(productId);
+		this.cartProducts.insertAdjacentHTML('beforeEnd',
 			`<div class="cart__product" data-id="${productId}">
 			<img class="cart__product-image" src="${image}">
 			<div class="cart__product-count">${quantity}</div>
 			<div class="cart__product-delete">&#x2716;</div>
-			</div>`
+			</div></div>`
 		);
 
 		this.checkCart();
-		this.deleteBtns.forEach(btn => {
-			btn.addEventListener('click', () => {
-				btn.parentElement.remove();
-			});
+		this.cartProducts.addEventListener('click', event => {
+			if (!event.target.closest('.cart__product-delete')) {
+				return;
+			}
+
+			const currentId = event.target.parentElement.getAttribute('data-id');
+
+			this.productId = this.productId.filter(id => id !== currentId);
+			event.target.closest('.cart__product').remove();
+			this.checkCart();
 		});
+
+		return this.cartProducts;
 	}
 }
 
@@ -86,19 +95,79 @@ class Product {
 		});
 
 		this.productQuantityInc.addEventListener('click', () => {
+
 			counter++;
 			this.productQuantityValue.textContent = counter;
 		});
 	}
 
 	addToCart() {
-		this.addBtn.addEventListener('click', () => {
-			Cart.addProduct(
-				this.product.getAttribute('data-id'),
-				this.product.querySelector('.product__image').getAttribute('src'),
-				+this.productQuantityValue.textContent
+		this.product.addEventListener('click', event => {
+			if (!event.target.closest('.product__add')) {
+				return;
+			}
+
+			const productId = this.product.getAttribute('data-id');
+			const image = this.product.querySelector('.product__image').getAttribute('src');
+			const quantity = +this.productQuantityValue.textContent;
+			const cartProducts = Cart.addProduct(
+				productId,
+				image,
+				quantity
 			);
+
+			this.moveCartAnimation(cartProducts, productId, image);
 		});
+	}
+
+	moveCartAnimation(cartProducts, productId, image) {
+		this.product.insertAdjacentHTML('beforeEnd', 
+			`<img class="cart__product-image cart__product-image_animated" src="${image}">`
+		);
+
+		const productImagePosition = this.product
+				.querySelector('.product__image')
+				.getBoundingClientRect();
+				
+			console.log(productImagePosition);
+
+		const cartImagePosition = cartProducts
+			.querySelector(`[data-id="${productId}"]`)
+			.querySelector('.cart__product-image')
+			.getBoundingClientRect();
+			
+		console.log(cartImagePosition);
+
+		const cartImageAnimated = this.product.lastChild;
+
+		console.log(cartImageAnimated);
+
+		let prTop = productImagePosition.top - 13;
+		let prLeft = productImagePosition.left;
+		const cartTop = cartImagePosition.top;
+		const cartLeft = cartImagePosition.left;
+
+		const prTopStatic = prTop;
+		const prLeftStatic = prLeft;
+
+		cartImageAnimated.style.top = `${prTop}px`;
+		cartImageAnimated.style.left = `${prLeft}px`;
+
+		const frames = 240;
+
+		let intervalId = setInterval(() => {
+			if (prLeft >= cartLeft || prTop <= cartTop) {
+				clearInterval(intervalId);
+				cartImageAnimated.remove();
+				return;
+			}
+
+			cartImageAnimated.style.top = `${prTop}px`;
+			cartImageAnimated.style.left = `${prLeft}px`;
+
+			prLeft += (cartLeft - prLeftStatic) * 5 / frames;
+			prTop -= (prTopStatic - cartTop) * 5 / frames;
+		}, 1000 / frames);
 	}
 }
 
